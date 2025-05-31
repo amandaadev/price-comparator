a8cde233d57538f53117874e79eaeb0afacd39b4
Amanda Hellen <amanda.hellen@g.globo>
2025-05-31 00:59:21 -0300

    feat: adjusting light/dark mode style

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -21,14 +21,13 @@ export const ProductList: React.FC<ProductListProps> = ({
     <ul className="mt-4 relative z-10">
       {resultados.map((item) => {
         const isMenorPreco = item.preco === menorPreco;
-
         return (
           <li
             key={item._id}
-            className={`border-2 p-2 mt-2 w-full text-center
+            className={`border-2 p-2 mt-2 w-full text-center text-white
             ${
               isMenorPreco
-                ? "border-yellow-400 bg-yellow-600 animate-pulse font-bold scale-105 text-black dark:text-white"
+                ? "border-yellow-400 bg-yellow-600 animate-pulse font-bold scale-105 text-white dark:text-white"
                 : "border-white text-black dark:text-white"
             }`}
           >
